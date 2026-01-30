1da8b5bdf01274f48ad9e26283dcaedd444f17bb
Prathmesh Hande <162908003+Prathmesh21H@users.noreply.github.com>
2026-01-30 15:02:22 +0530

    Update CORS origin to new URL

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ["https://digital-card-9pov.onrender.com"],
+    origin: ["https://digital-card-beta-six.vercel.app"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
   })
